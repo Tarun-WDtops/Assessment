c999fdb8f4e52856d22e261155aceee7dbd8c132
Tarun-WDtops <tjparmar786@gmail.com>
2024-02-16 15:48:12 +0530

    assessment added

--- jQuery_Assesment/javaSript.js
+++ jQuery_Assesment/javaSript.js
@@ -1,6 +1,46 @@
-function one() {
-  const inputEl = document.getElementById("first-input");
-  const value = inputEl.getAttribute("value") || "";
-  const newValue = value + "1";
-  inputEl.setAttribute("value", newValue);
+function setValueone() {
+  let tarun = parseInt((document.getElementById("first-input").value = "1"));
+}
+
+function setValuetwo() {
+  let tarun = parseInt((document.getElementById("first-input").value = "2"));
+}
+
+function setValuethree() {
+  let tarun = parseInt((document.getElementById("first-input").value = "3"));
+}
+
+function setValuefour() {
+  let tarun = parseInt((document.getElementById("first-input").value = "4"));
+}
+
+function setValuefive() {
+  let tarun = parseInt((document.getElementById("first-input").value = "5"));
+}
+
+function setValuesix() {
+  let tarun = parseInt((document.getElementById("first-input").value = "6"));
+}
+
+function setValueseven() {
+  let tarun = parseInt((document.getElementById("first-input").value = "7"));
+}
+
+function setValueeight() {
+  let tarun = parseInt((document.getElementById("first-input").value = "8"));
+}
+
+function setValuenine() {
+  let tarun = parseInt((document.getElementById("first-input").value = "9"));
+}
+
+function setValuezero() {
+  let tarun = parseInt((document.getElementById("first-input").value = "0"));
+}
+function setValuedot() {
+  let tarun = parseInt((document.getElementById("first-input").value = "."));
+}
+
+function del() {
+  let a = (document.getElementById("first-input").value = "");
 }
